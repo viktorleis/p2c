9d6368f433bbd2bb379339d30db1ca5bf0ebd6f3
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2025-10-21 12:25:55 +0200

    Delete old input to reduce maximum disk usage

--- data-generator/generate-data.sh
+++ data-generator/generate-data.sh
@@ -2,13 +2,12 @@
 
 sf=${1:-"1"}
 
-mkdir -p input
+rm -rf input && mkdir -p input
 ( # generate csv data
   cd dbgen || exit 1
   test -f dbgen || ( git submodule update --init .; make target=release )
   ./dbgen -C"$sf" -s"$sf"
   chmod uga+rw-x *.tbl
-  rm ../input/*
   mv -f ./*.tbl* ../input
 ) || exit $?
 # convert csv data to binary
